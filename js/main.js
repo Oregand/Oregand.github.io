87ca044977d76143665262e0f7913a6b87fdb5d4
Oregand <davidoregan91@gmail.com>
2016-02-03 17:21:40 +0000

    [impl-NA] Update With Base Structure For Landing Page

--- js/main.js
+++ js/main.js
@@ -28,17 +28,6 @@
     'coming_soon': {
       init: function() {
         // JavaScript to be fired on the coming soon page
-        var time = 10;
-        var initialOffset = '440';
-        var i = 1;
-        var interval = setInterval(function() {
-            $('.circle_animation').css('stroke-dashoffset', initialOffset-(i*(initialOffset/time)));
-            $('h2').text(i);
-            if (i == time) {
-                clearInterval(interval);
-            }
-            i++;
-        }, 1000);
       },
       finalize: function() {
         // JavaScript to be fired on the coming soon page, after the init JS
