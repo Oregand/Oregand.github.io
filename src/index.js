d6cdf316d3b369b59d37131af8681dc70292363e
David <doregan@vsware.ie>
2018-10-04 21:12:41 +0100

    Lets get back to basics - thank you Dan

--- src/index.js
+++ src/index.js
@@ -1,45 +1,12 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { ConnectedRouter } from 'react-router-redux'
-import { PersistGate } from 'redux-persist/integration/react'
-import store, { history, persistor } from './state/store'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import App from './containers/App'
-import registerServiceWorker from './registerServiceWorker'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css'
+ReactDOM.render(<App />, document.getElementById('root'));
 
-const target = document.querySelector('#root')
-
-render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <ConnectedRouter history={history}>
-        <div>
-          <App />
-        </div>
-      </ConnectedRouter>
-    </PersistGate>
-  </Provider>,
-  target
-)
-
-if (module.hot) {
-  module.hot.accept('./containers/App', () => {
-    render(
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <ConnectedRouter history={history}>
-            <div>
-              <App />
-            </div>
-          </ConnectedRouter>
-        </PersistGate>
-      </Provider>,
-      target
-    )
-  })
-}
-
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
--- src/state/modules/login/utils.js
+++
@@ -1,2 +0,0 @@
-export const NEW_SESSION = 'login/NEW_SESSION'
-export const LOGIN_STATE_KEY = 'login'
