c14af70b34346ea2043392523e2424d1de4dd080
David <doregan@vsware.ie>
2018-10-05 21:39:47 +0100

    Updates

---
+++ precache-manifest.c51063f1f4473c2b4f8f7276bd8d0f1a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "53fa00176bad87a00377449c19474893",
+    "url": "/oregand.gitgub.io/static/media/welcome-cover.53fa0017.jpg"
+  },
+  {
+    "revision": "be76db2886a25a39b65f66883c0528f3",
+    "url": "/oregand.gitgub.io/static/media/map.be76db28.jpg"
+  },
+  {
+    "revision": "4b5c2260b3be30f223d2bfb15e61c8b9",
+    "url": "/oregand.gitgub.io/static/media/features-background.4b5c2260.jpg"
+  },
+  {
+    "revision": "5127f0562f9026cee741fd1b6763e531",
+    "url": "/oregand.gitgub.io/static/media/david.5127f056.jpg"
+  },
+  {
+    "revision": "a4e02892b51edc6939c0",
+    "url": "/oregand.gitgub.io/static/js/runtime~main.a4e02892.js"
+  },
+  {
+    "revision": "cba7241e066f25ea2ecd",
+    "url": "/oregand.gitgub.io/static/js/main.cba7241e.chunk.js"
+  },
+  {
+    "revision": "504be42e907831859c13",
+    "url": "/oregand.gitgub.io/static/js/1.504be42e.chunk.js"
+  },
+  {
+    "revision": "cba7241e066f25ea2ecd",
+    "url": "/oregand.gitgub.io/static/css/main.4404285d.chunk.css"
+  },
+  {
+    "revision": "504be42e907831859c13",
+    "url": "/oregand.gitgub.io/static/css/1.b0f4a3a3.chunk.css"
+  },
+  {
+    "revision": "389eb8cf59f55024cc50492aa900efa8",
+    "url": "/oregand.gitgub.io/index.html"
+  }
+];
